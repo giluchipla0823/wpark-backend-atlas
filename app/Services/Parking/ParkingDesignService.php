a6578135c22c1a26c1634f7a0558fe34d3dbccf1
Gino Luiggi Chirinos Plasencia <lchirinos@acercapartners.com>
2022-05-11 12:50:47 +0200

    cambios

--- app/Services/Parking/ParkingDesignService.php
+++ app/Services/Parking/ParkingDesignService.php
@@ -77,8 +77,10 @@ class ParkingDesignService
 
             // Calcular la capacidad del parking
             $params['capacity'] = 0;
-            foreach($params['rows'] as $row){
-                $params['capacity'] += $row['slots'];
+            if ($params['parking_type_id'] != ParkingType::TYPE_UNLIMITED) {
+                foreach($params['rows'] as $row){
+                    $params['capacity'] += $row['slots'];
+                }
             }
             $params['capacity'] = $params['parking_type_id'] != ParkingType::TYPE_UNLIMITED ? $params['capacity'] : null;
             $params['fill'] = $params['parking_type_id'] != ParkingType::TYPE_UNLIMITED ? 0 : null;
