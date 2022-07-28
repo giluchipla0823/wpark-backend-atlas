072c285f9c574c8d21948405de2bcefeafb70597
Gino Luiggi Chirinos Plasencia <lchirinos@acercapartners.com>
2022-07-28 09:32:46 +0200

    Cambios

--- app/Repositories/Movement/Builders/MovementDatatablesQueryBuilder.php
+++ app/Repositories/Movement/Builders/MovementDatatablesQueryBuilder.php
@@ -6,6 +6,7 @@ use App\Models\Parking;
 use App\Models\Movement;
 use App\Helpers\DatesHelper;
 use App\Helpers\QueryHelper;
+use App\Models\Slot;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Query\Builder as QueryBuilder;
@@ -38,50 +39,21 @@ class MovementDatatablesQueryBuilder
      */
     public function getQuery(): EloquentBuilder
     {
-        $parkingModel = QueryHelper::escapeNamespaceClass(Parking::class);
-
-        // Creamos la subquery de parkings.
-        $subQuery = $this->getSubQueryParkings();
-
         $this->query = $this->model->query()
-                        ->select([
-                            "movements.*",
-                            DB::raw("
-                            (
-                                CASE
-                                    WHEN movements.confirmed = 1 THEN 'confirmed'
-                                    WHEN movements.canceled = 1 THEN 'canceled'
-                                    ELSE 'pending'
-                                END
-                            ) AS `status`
-                            "),
-                            "vehicles.id AS vehicle_id",
-                            "vehicles.vin AS vehicle_vin",
-                            "devices.id AS device_id",
-                            "devices.name AS device_name",
-                            "devices.uuid AS device_uuid",
-                            "users.id AS user_id",
-                            "users.username AS user_username",
-                            "parking_origin.parking_row AS origin_position_name",
-                            "parking_origin.parking_id AS origin_parking_id",
-                            "parking_origin.parking_name AS origin_parking_name",
-                            "parking_origin.row_id AS origin_row_id",
-                            "parking_origin.row_number AS origin_row_number",
-                            "parking_origin.slot_id AS origin_slot_id",
-                            "parking_origin.slot_number AS origin_slot_number",
-                            "parking_destination.parking_row AS destination_position_name",
-                            "parking_destination.parking_id AS destination_parking_id",
-                            "parking_destination.parking_name AS destination_parking_name",
-                            "parking_destination.row_id AS destination_row_id",
-                            "parking_destination.row_number AS destination_row_number",
-                            "parking_destination.slot_id AS destination_slot_id",
-                            "parking_destination.slot_number AS destination_slot_number"
-                        ])
-                        ->join("vehicles", "movements.vehicle_id", "=", "vehicles.id")
-                        ->join("users", "movements.user_id", "=", "users.id")
-                        ->leftJoin("devices", "movements.device_id", "=", "devices.id")
-                        ->leftJoin(DB::raw("({$subQuery->toSql()}) AS parking_origin"), "movements.origin_position_id", "=", DB::raw("IF (movements.origin_position_type = '{$parkingModel}', parking_origin.parking_id, parking_origin.slot_id)"))
-                        ->leftJoin(DB::raw("({$subQuery->toSql()}) AS parking_destination"), "movements.destination_position_id", "=", DB::raw("IF (movements.destination_position_type = '{$parkingModel}', parking_destination.parking_id, parking_destination.slot_id)"));
+            ->from(DB::raw("({$this->getFromTable()}) AS movements"))
+            ->select([
+                "movements.*",
+                "vehicles.id AS vehicle_id",
+                "vehicles.vin AS vehicle_vin",
+                "devices.id AS device_id",
+                "devices.name AS device_name",
+                "devices.uuid AS device_uuid",
+                "users.id AS user_id",
+                "users.username AS user_username",
+            ])
+            ->join("vehicles", "movements.vehicle_id", "=", "vehicles.id")
+            ->join("users", "movements.user_id", "=", "users.id")
+            ->leftJoin("devices", "movements.device_id", "=", "devices.id");
 
         $this->addFilters();
 
@@ -89,26 +61,8 @@ class MovementDatatablesQueryBuilder
     }
 
     /**
-     * @return QueryBuilder
-     */
-    private function getSubQueryParkings(): QueryBuilder
-    {
-        return DB::table("parkings", "p")
-            ->select([
-                "p.id AS parking_id",
-                "p.name AS parking_name",
-                "r.id AS row_id",
-                "r.row_number AS row_number",
-                "s.id AS slot_id",
-                "s.slot_number AS slot_number",
-                DB::raw("CONCAT(p.name, IF(r.row_number IS NOT NULL, CONCAT('.', LPAD(r.row_number, 3, '0')), '')) AS parking_row"),
-                DB::raw("CONCAT(p.name, IF(r.row_number IS NOT NULL, CONCAT('.', LPAD(r.row_number, 3, '0')), ''), IF(s.id IS NOT NULL, CONCAT('-', s.slot_number), '')) AS parking_row_slot")
-            ])
-            ->leftJoin("rows AS r", "p.id", "=", "r.parking_id")
-            ->leftJoin("slots AS s", "r.id", "=", "s.row_id");
-    }
-
-    /**
+     * Agregar filtros a la query según parámetros enviados por la petición.
+     *
      * @return void
      */
     private function addFilters(): void
@@ -135,14 +89,14 @@ class MovementDatatablesQueryBuilder
         if ($originsParkings = $this->request->get('origins_parkings')) {
             $originsParkings = explode(',', $originsParkings);
 
-            $this->query = $this->query->whereIn('parking_origin.parking_id', $originsParkings);
+            $this->query = $this->query->whereIn('movements.origin_parking_id', $originsParkings);
         }
 
         // Destinations parkings filter
         if ($destinationsParkings = $this->request->get('destinations_parkings')) {
             $destinationsParkings = explode(',', $destinationsParkings);
 
-            $this->query = $this->query->whereIn('parking_destination.parking_id', $destinationsParkings);
+            $this->query = $this->query->whereIn('movements.destination_parking_id', $destinationsParkings);
         }
 
         // Created_at filter
@@ -151,5 +105,71 @@ class MovementDatatablesQueryBuilder
 
             $this->query = $this->query->whereBetween("movements.created_at", [$startDate, $endDate]);
         }
+    }
+
+    /**
+     * Obtener query de movements para la claúsula "from" para la consulta principal.
+     *
+     * @return string
+     */
+    private function getFromTable(): string
+    {
+        $slotModel = QueryHelper::escapeNamespaceClass(Slot::class);
+        $parkingModel = QueryHelper::escapeNamespaceClass(Parking::class);
+
+        $parkingSubQuery = DB::table("parkings", "p")
+            ->select([
+                "p.id AS parking_id",
+                "p.name AS parking_name",
+                "r.id AS row_id",
+                "r.row_number AS row_number",
+                "s.id AS slot_id",
+                "s.slot_number AS slot_number",
+                DB::raw("CONCAT(p.name, IF(r.`row_number` IS NOT NULL, CONCAT('.', LPAD(r.`row_number`, 3, '0')), '')) AS parking_row")
+            ])
+            ->leftJoin("rows AS r", "p.id", "=", "r.parking_id")
+            ->leftJoin("slots AS s", "r.id", "=", "s.row_id");
+
+        $query = "
+            SELECT
+                movements.*,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.parking_row, parking_origin.name) AS origin_position_name,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.parking_id, parking_origin.id) AS origin_parking_id,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.parking_name, parking_origin.name) AS origin_parking_name,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.row_id, null) AS origin_row_id,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.`row_number`, null) AS origin_row_number,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.slot_id, null) AS origin_slot_id,
+                IF(movements.origin_position_type = ':slot_model', slots_origin.slot_number, null) AS origin_slot_number,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.parking_row, parking_destination.name) AS destination_position_name,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.parking_id, parking_destination.id) AS destination_parking_id,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.parking_name, parking_destination.name) AS destination_parking_name,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.row_id, null) AS destination_row_id,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.`row_number`, null) AS destination_row_number,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.slot_id, null) AS destination_slot_id,
+                IF(movements.destination_position_type = ':slot_model', slots_destination.slot_number, null) AS destination_slot_number,
+                (
+                    CASE
+                        WHEN movements.confirmed = 1 THEN 'confirmed'
+                        WHEN movements.canceled = 1 THEN 'canceled'
+                        ELSE 'pending'
+                    END
+                ) AS `status`
+            FROM
+                movements
+            LEFT JOIN
+            (
+                {$parkingSubQuery->toSql()}
+            ) AS slots_origin ON movements.origin_position_id = slots_origin.slot_id and movements.origin_position_type = ':slot_model'
+            LEFT JOIN
+            (
+                {$parkingSubQuery->toSql()}
+            ) AS slots_destination ON movements.destination_position_id = slots_destination.slot_id and movements.destination_position_type = ':slot_model'
+            LEFT JOIN
+                parkings AS parking_origin ON movements.origin_position_id = parking_origin.id AND movements.origin_position_type = ':parking_model'
+            LEFT JOIN
+                parkings AS parking_destination ON movements.destination_position_id = parking_destination.id AND movements.destination_position_type = ':parking_model'
+        ";
+
+        return str_replace([":slot_model", ":parking_model"], [$slotModel, $parkingModel], $query);
     }
 }
